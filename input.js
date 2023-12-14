908c26a05e8e82b61df7190842b943caecd56822
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2023-12-14 17:59:43 +0500

    jksdfjsdnfjksd

--- input.js
+++ input.js
@@ -4,7 +4,8 @@ const input = process.argv
 if(input[2]=='add'){
     fs.writeFileSync(input[3],input[4])
 }
-else if(input[2]=='remove'){
+else 
+if(input[2]=='remove'){
     fs.unlinkSync(input[3])
 }
 else(
--- path.js
+++ path.js
@@ -1,26 +1,41 @@
-const fs = require('fs')
-const path= require('path')
+// const fs = require('fs')
+// const path= require('path')
 
-const dirpath = path.join(__dirname,'files')
-for(i=0;i<=5;i++){
-    fs.writeFileSync(`${dirpath}/new${i}.js`,'hwell')
-}
+// const dirpath = path.join(__dirname,'files')
+// for(i=0;i<=5;i++){
+//     fs.writeFileSync(`${dirpath}/new${i}.js`,'hwell')
+// }
+
+// // fs.readdir(dirpath,(err,file)=>{
+// // if(file){
+// //     file.forEach((item)=>{
+// //         console.log(item)
+// //     })
+// // }
+// // else{
+// //  console.log(err+'errr')
+// // }
+// // })
 
 // fs.readdir(dirpath,(err,file)=>{
-// if(file){
-//     file.forEach((item)=>{
-//         console.log(item)
+ 
+//         file.forEach((item)=>{
+//             console.log(item)
+//         })
+    
 //     })
-// }
-// else{
-//  console.log(err+'errr')
-// }
-// })
+
+const path = require('path')
+const fs = require('fs')
+const dirpath = path.join(__dirname,'files')
+console.log(dirpath)
+
+for(i=0;i<=5;i++){
+    fs.writeFileSync(`${dirpath}/hello${i}.js`,'helo')
+}
 
 fs.readdir(dirpath,(err,file)=>{
- 
-        file.forEach((item)=>{
-            console.log(item)
-        })
-    
-    })
+   file.forEach((item)=>{
+    console.log(item)
+   })
+})
