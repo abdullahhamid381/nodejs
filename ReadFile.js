9958cb011743298e774a657d7d021fd5c4059d81
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2024-01-01 14:44:57 +0500

    mmdsm

--- ReadFile.js
+++ ReadFile.js
@@ -2,6 +2,5 @@ const fs = require('fs');
 const path = require('path')
 
 const dirpath = path.join(__dirname,'File')
-for(i=0;i<=10;i++){
-    fs.unlinkSync(`${dirpath}/app${i}.js`,'hellooo')
-}
+
+    fs.writeFileSync(`${dirpath}/app.js`,'hellooo')
